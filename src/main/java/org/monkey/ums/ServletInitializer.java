21bd08d811961304f4cc03d10f95fb5401390354
monkey <monkey_win@sina.com>
2018-11-07 00:32:12 +0800

    update ServletInitializer.java

--- src/main/java/org/monkey/ums/ServletInitializer.java
+++ src/main/java/org/monkey/ums/ServletInitializer.java
@@ -7,7 +7,9 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		/*11111111*/
         return application.sources(UmsApplication.class);
+		/*asdfasdf*/
     }
 
 }
